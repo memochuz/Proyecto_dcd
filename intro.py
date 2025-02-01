8d77950470eab1dc70d5c792e40c085ab886a872
memochuz <87551547+memochuz@users.noreply.github.com>
2025-02-01 16:24:57 +0000

    Modifiqué la intro agregué  contexto, problema y objetivo

--- intro.py
+++ intro.py
@@ -15,7 +15,21 @@ def intro_page():
    # # Mostrar el contenido del archivo markdown en Streamlit
    # st.markdown("---")
    # st.markdown(contenido_markdown)
+    st.markdown(
+        """
+        ### Proyecto: Optimización de Gestión para Tienda Online  
 
+        #### Contexto  
+        Un emprendedor con una tienda online en **WhatsApp** especializada en **zapatos y accesorios** experimentó un crecimiento en su negocio.  
+
+        #### Problema  
+        La falta de una **estructura sólida de registro de compras** dificultaba el **análisis del negocio** y el **diseño de estrategias de crecimiento**.  
+
+        #### Objetivo del Proyecto  
+        - Implementar un **sistema de gestión de compras** para mejorar el **seguimiento de pedidos y análisis de ventas**.  
+        - Facilitar la toma de decisiones basada en **datos estructurados**.  
+        """
+        )
     col1, col2, col3 = st.columns([1,3,1])
     with col2:
         st.image("BMC_V.png")
