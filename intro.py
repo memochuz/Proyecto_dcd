235f3a8b73d33648479d3d58f5fc830f180b2b75
memochuz <87551547+memochuz@users.noreply.github.com>
2025-02-01 02:15:57 +0000

    Agregar imágnes a la intro

--- intro.py
+++ intro.py
@@ -3,15 +3,27 @@ import streamlit as st
 def intro_page():
     st.header("Página: Descripción del Proyecto")
 
-    col1, col2 , col3 = st.columns([1,8,1])
+   # col1, col2 , col3 = st.columns([1,8,1])
+   # with col2:
+   #     st.image("BMC.png")
+   #     st.image("MLC.png")
+   #
+   # # Leer el archivo markdown
+   # with open("flujo_proyecto.md", "r") as file:
+   #     contenido_markdown = file.read()
+   #
+   # # Mostrar el contenido del archivo markdown en Streamlit
+   # st.markdown("---")
+   # st.markdown(contenido_markdown)
+
+    col1, col2, col3 = st.columns([1,3,1])
     with col2:
-        st.image("BMC.png")
-        st.image("MLC.png")
-
-    # Leer el archivo markdown
-    with open("flujo_proyecto.md", "r") as file:
-        contenido_markdown = file.read()
-
-    # Mostrar el contenido del archivo markdown en Streamlit
+        st.image("BMC_V.png")
     st.markdown("---")
-    st.markdown(contenido_markdown)
+    col1, col2, col3 = st.columns([1,3,1])
+    with col2:
+        st.image("MLC_V.png")
+    st.markdown("---")
+    col1, col2, col3 = st.columns([1,3,1])
+    with col2:
+        st.image("FLUJO_V.png")
